7c837cdb67ec3335eb52f2499f57a503c607918f
Art0ur <56406313+Art0ur@users.noreply.github.com>
2024-06-11 11:59:53 -0300

    backend ok

---
+++ backend/src/main/java/com/backend/controllers/PessoaController.java
@@ -0,0 +1,80 @@
+package com.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.dto.PessoaDTO;
+import com.backend.exceptions.NotFoundException;
+import com.backend.model.Pessoa;
+import com.backend.service.PessoaService;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody PessoaDTO dto) {
+        try {
+            var res = pessoaService.create(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(res);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping
+    public List<Pessoa> getAll() {
+        return pessoaService.getAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable("id") long id) {
+        var person = pessoaService.getById(id);
+        return person.isPresent() ? ResponseEntity.ok().body(person.get()) : ResponseEntity.notFound().build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable("id") long id, @RequestBody PessoaDTO dto) {
+        try {
+            return ResponseEntity.ok().body(pessoaService.update(id, dto));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") long id) {
+        try {
+            pessoaService.delete(id);
+            return ResponseEntity.ok().build();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
